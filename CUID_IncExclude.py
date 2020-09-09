4fb31c16b4cadbbaeea8fd1f991972e3e7644ce6
Steve <MCTyler@users.noreply.github.com>
2020-09-09 00:12:02 -0500

    Update CUID_IncExclude.py
    Add User Agent Headers and Include M3U Groups that you select (Edit Examples Provided to Fit your M3U `group-title="Blah"` Also Rename the file CUID_Include.py.

--- CUID_IncExclude.py
+++ CUID_IncExclude.py
@@ -6,7 +6,7 @@ import os
 url = 'http://providerURL'
 #File path where to save the newly create M3U file.
 filePath = 'updatedm3ufile.m3u'
-#Headers
+#Headers VLC Standard, Change to what you like....
 headers = {'User-Agent': 'VLC'}
 
 #Combine URL and Headers into one
@@ -26,8 +26,8 @@ m3uLines = providerM3uFile.split('#EXTINF:-1')
 del m3uLines[0]
 newM3uFile = '#EXTM3U' + '\n'
 for m3uLine in m3uLines:
-    #Filter channels that you want to include by group title. 
-    #This assumes that your provider uses the group-title= property to identify gropus, if not change it
+    #Filter channels that you want to ***include*** by group title. 
+    #This assumes that your provider uses the group-title= property to identify groups, if not change it
     if ('group-title="NETWORK TV"' in m3uLine) or ('group-title="REGIONAL LOCALS"' in m3uLine) or ('group-title="SPORTS NETWORKS"' in m3uLine) or ('group-title="NFL/NCAAF"' in m3uLine) or ('group-title="PPV / EVENTS"' in m3uLine) or ('group-title="NBA"' in m3uLine) or ('group-title="24/7 TV"' in m3uLine):
         channelInfo = m3uLine.splitlines()[0].strip()
         channelUrl = m3uLine.splitlines()[1].strip()
